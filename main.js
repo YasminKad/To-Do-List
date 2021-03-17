f23b706a0888ebb68870c0b0871496370b533a74
yasminkadkhodaei <yasmin.kadkhodaei@gmail.com>
2021-03-17 15:36:21 +0330

    sort function for the main display added

--- main.js
+++ main.js
@@ -1,116 +1,156 @@
-const data = {todoList: [{id: 0, title: "task", color: "colorName", priority: "cardPriority", state: "undone", isPinned: false}]}
-let todoListCopy = data.todoList;
-let dependencies = [];
+const data = {
+    todoList: [{
+        id: 0,
+        title: "task",
+        color: "colorName",
+        priority: "low",
+        state: "done",
+        isPinned: false
+    }]
+}
+const priorityValues = {high: 2, medium: 1, low: 1}
+let todoListCopy = data.todoList
+let dependencies = []
 
 Object.defineProperty(data, 'todoList', {
     get() {
-        trackItem();
-        return todoListCopy;
+        trackItem()
+        return todoListCopy
     },
     set(value) {
-        todoListCopy = value;
-        triggerItem();
+        todoListCopy = value
+        triggerItem()
     }
 })
 
 function trackItem() {
     if (currDependency) {
-        dependencies.push(currDependency);
+        dependencies.push(currDependency)
     }
 }
 
 function triggerItem() {
-    dependencies.forEach(fn => fn());
+    dependencies.forEach(fn => fn())
 }
 
-let currDependency = null;
+let currDependency = null
 
 function observe(fn) {
-    currDependency = fn;
-    fn();
-    currDependency = null;
+    currDependency = fn
+    fn()
+    currDependency = null
 }
 
 function dep() {
     // console.log(data.todoList)
-    return data.todoList;
+    return data.todoList
 }
+
 observe(dep);
 
- function getArrayCopy(){
-     data.todoList = [...todoListCopy]
- }
+function getArrayCopy() {
+    data.todoList = [...todoListCopy]
+}
 
 function addTodo(cardName, cardColor, cardPriority) {
-    const todo = {id: Date.now(), title: cardName, color: cardColor, priority: cardPriority, state: "undone"};
+    const todo = {id: Date.now(), title: cardName, color: cardColor, priority: cardPriority, state: "undone"}
     data.todoList = [...todoListCopy, todo]
 }
 
 console.log("add to do test;")
-addTodo("yasmin", "white", "high");
-addTodo("ahmadReza", "black", "medium");
-addTodo("ehsan", "yellow", "low");
+addTodo("yasmin", "white", "high")
+addTodo("ahmadReza", "black", "medium")
+addTodo("ehsan", "yellow", "medium")
 console.log(data.todoList)
 
 function removeTodo(id) {
-    for (let i = 0; i < data.todoList.length ; i++) {
-        if( data.todoList[i].id === id ){
-            todoListCopy.splice(i, 1);
-            getArrayCopy();
-            break;
+    for (let i = 0; i < data.todoList.length; i++) {
+        if (data.todoList[i].id === id) {
+            todoListCopy.splice(i, 1)
+            getArrayCopy()
+            break
         }
     }
 }
+
 console.log("remove test")
-removeTodo(data.todoList[1]);
+removeTodo(data.todoList[1])
 
 function changeState(id) {
     for (let todoListElement of data.todoList) {
         if (todoListElement.id === id) {
             if (todoListElement.state === "done") {
-                todoListElement.state = "undone";
-                getArrayCopy();
+                todoListElement.state = "undone"
+                getArrayCopy()
             }
-            if (todoListElement.state === "undone" ){
-                todoListElement.state = "done";
-                getArrayCopy();
+            if (todoListElement.state === "undone") {
+                todoListElement.state = "done"
+                getArrayCopy()
                 console.log("state changed")
-                let temp;
-                temp = todoListElement;
-                removeTodo(id);
-                data.todoList =  [...todoListCopy, temp]
+                let temp
+                temp = todoListElement
+                removeTodo(id)
+                data.todoList = [...todoListCopy, temp]
             }
             break;
         }
     }
 }
-console.log("change state test")
-changeState(data.todoList[2].id);
 
-function pin(id){
+console.log("change state test")
+changeState(data.todoList[2].id)
+
+function pin(id) {
     for (let todoListElement of data.todoList) {
         if (todoListElement.id === id) {
-            let temp;
-            temp = todoListElement;
-            temp.isPinned = true;
-            removeTodo(id);
+            let temp
+            temp = todoListElement
+            temp.isPinned = true
+            removeTodo(id)
             data.todoList = [temp, ...todoListCopy]
             console.log(data.todoList)
             console.log("pinned successfully")
-            break;
+            break
         }
     }
 }
-console.log("pin test")
-pin(data.todoList[1].id);
 
-// function sort(filter){
-//     for (const todoListElement of data.todoList) {
-//         if (todoListElement.isPinned === false) {
-//
-//         }
-//     }
-// }
-// function costumeFilter(){
-//
-// }
+console.log("pin test")
+pin(data.todoList[1].id)
+
+function sort() {
+    const {pinned, notPinned, done} = data.todoList.reduce((prev, curr) => {
+        if (curr.isPinned) {
+            return {...prev, pinned: [...prev.pinned, curr]}
+        } else if(curr.state === "done"){
+            return {...prev, done: [...prev.done, curr]}
+        }
+        else {
+            return {...prev, notPinned: [...prev.notPinned, curr]}
+        }
+    }, {pinned: [], notPinned: [], done: []})
+    const sortedList = notPinned.sort((first, second) => {
+            if (priorityValues[first.priority] > priorityValues[second.priority]) {
+                console.log("return 1");
+                return -1
+            }
+            if (priorityValues[first.priority] < priorityValues[second.priority]) {
+                console.log("return -1");
+                return 1
+            } else {
+                console.log("return 0");
+                return 0
+            }
+        }
+    )
+        data.todoList = [...pinned, ...sortedList, ...done]
+    console.log(data.todoList)
+}
+
+sort();
+
+function costumeFilter() {
+
+}
+
+costumeFilter()
